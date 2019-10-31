bbb74717e16410d580577f67477eb7698a9cb6dd
chrisw000 <18504164+chrisw000@users.noreply.github.com>
2019-10-31 08:02:35 +0000

    Tidy up the js / lobby view

--- BigCheese/wwwroot/js/lobby.js
+++ BigCheese/wwwroot/js/lobby.js
@@ -2,74 +2,98 @@
 
 var gameConnection = new signalR.HubConnectionBuilder().withUrl("/hub/lobby").build();
 
+function Lobby () {
+    this.player =  null;
+    this.game = null;
+
+    this.newPlayer = function (playerData) {
+        this.player = playerData;
+        this.showJson("player", playerData);
+    };
+    this.joinGame = function (gameData) {
+        this.game = gameData;
+        this.showJson("game", gameData);
+    };
+    this.updateGame = function (gameData) {
+        this.game = gameData;
+        this.showJson("game", gameData);
+    };
+    this.showJson = function (name, obj) {
+        console.log(obj);
+        document.getElementById(name+"Json").innerHTML = JSON.stringify(obj);
+    };
+};
+
+let lobby = new Lobby();
+
 //Disable send button until connection is established
 document.getElementById("lobbyNewGameButton").disabled = true;
 document.getElementById("lobbyJoinGameButton").disabled = true;
 
 gameConnection.on("LobbyNewGameHasStarted", function (gameData) {
-    /*
-     * gameData is properties as per BlueCheese.HostedServices.Bingo.IGameData
-     */
-    showJson(gameData);
+
+    // TODO: maintain the full list of games
+
     document.getElementById("lobbyJoinGameId").value = gameData.gameId;
+    document.getElementById("lobbyJoinGameButton").disabled = !(lobby.player && !lobby.game);
+
     addMessageToUIQueue("New Game Started: " + gameData.gameId);
 });
 
 gameConnection.on("LobbyUserJoinedGame", function (gameData, message) {
-    /*
-     * gameData is properties as per BlueCheese.HostedServices.Bingo.IGameData
-     */
-    showJson(gameData);
+
+    lobby.joinGame(gameData);
 
     addMessageToUIQueue(user + " joined game " + gameData.gameId + " > " + message);
 });
 
 gameConnection.on("LobbyPlayerNumbers", function (gameData, player) {
-     /*
-     * gameData is properties as per BlueCheese.HostedServices.Bingo.IGameData
-     */
-    showJson(gameData);
-    console.log(player);
+
+    lobby.joinGame(gameData);
+
+    document.getElementById("lobbyNewGameButton").disabled = true;
+    document.getElementById("lobbyJoinGameButton").disabled = true;
 
     addMessageToUIQueue("My Player: " + JSON.stringify(player));
 });
 
 gameConnection.on("LobbyPlayerMessage", function (gameData, message) {
-     /*
-     * gameData is properties as per BlueCheese.HostedServices.Bingo.IGameData
-     */
-    showJson(gameData);
+
+    lobby.updateGame(gameData);
 
     addMessageToUIQueue(message);
 });
 
 gameConnection.on("LobbyUpdateGame", function (gameData, message) {
-     /*
-     * gameData is properties as per BlueCheese.HostedServices.Bingo.IGameData
-     */
-    showJson(gameData);
+
+    lobby.updateGame(gameData);
+
     document.getElementById("gamePulseMessage").innerHTML = message;
 });
 
 gameConnection.start().then(function () {
-    document.getElementById("lobbyNewGameButton").disabled = false;
-    document.getElementById("lobbyJoinGameButton").disabled = false;
+    if (document.getElementById("lobbyJoinGameId").value) {
+        document.getElementById("lobbyJoinGameButton").disabled = false;
+    } else {
+        document.getElementById("lobbyNewGameButton").disabled = false;
+    }
 }).catch(function (err) {
     return console.error(err.toString());
 });
 
 document.getElementById("lobbyNewGameButton").addEventListener("click", function (event) {
 
-    var user = document.getElementById("lobbyUsername").value;
-    var playerId = document.getElementById("playerId").value;
+    document.getElementById("lobbyNewGameButton").disabled = true;
+    document.getElementById("lobbyJoinGameButton").disabled = true;
+
     var name = document.getElementById("lobbyNewGameName").value;
     var mode = document.getElementById("lobbyNewGameMode").value;
     var size = document.getElementById("lobbyNewGameSize").value;   
     var cheeseCount = document.getElementById("lobbyNewGameCheeseCount").value;
 
     var newGame = {
-            user: user,
-            playerId: playerId,
+            user: lobby.player.user,
+            playerId: lobby.player.playerId,
             name: name,
             mode: parseInt(mode),
             size: parseInt(size),
@@ -88,13 +112,15 @@ document.getElementById("lobbyNewGameButton").addEventListener("click", function
 });
 
 document.getElementById("lobbyJoinGameButton").addEventListener("click", function (event) {
-    var user = document.getElementById("lobbyUsername").value;
-    var playerId = document.getElementById("playerId").value;
+
+    document.getElementById("lobbyNewGameButton").disabled = true;
+    document.getElementById("lobbyJoinGameButton").disabled = true;
+
     var gameId = document.getElementById("lobbyJoinGameId").value;
 
     var joinGame = {
-        user: user,
-        playerId: playerId,
+        user: lobby.player.user,
+        playerId: lobby.player.playerId,
         gameId: gameId
     };
 
@@ -109,10 +135,6 @@ function addMessageToUIQueue(message) {
     li.textContent = message;
     document.getElementById("messagesList").appendChild(li);
 }
-function showJson(gameData) {
-    console.log(gameData);
-    document.getElementById("gameJson").innerHTML = JSON.stringify(gameData);
-}
 
 //------------------------------------------------------------------------------------
 
@@ -122,8 +144,14 @@ document.getElementById("softLogOnButton").addEventListener("click", function (e
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-          document.getElementById("playerId").value = JSON.parse(this.responseText).playerId;
+
+          lobby.newPlayer(JSON.parse(this.responseText));
+
           document.getElementById("softLogOnButton").disabled = true;
+          document.getElementById("lobbyUsername").disabled = true;
+          document.getElementById("newGateDataEntry").style.display = "block";    
+            
+          document.getElementById("lobbyJoinGameButton").disabled = !(lobby.player && !lobby.game);
         }
       };
     xhttp.open("POST", "/api/account", true);
